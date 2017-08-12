45de65dd065cce7c6624eec1b68fb5fab71e4e1c
ronnie.o <ronnie.o@kakaocorp.com>
2017-08-12 15:50:34 +0900

    Day 8: Dictionaries and Maps

---
+++ 30-days-of-code/src/main/scala/com/hackerrank/day8/DictionaryAndMaps.scala
@@ -0,0 +1,28 @@
+package com.hackerrank.day8
+
+/**
+  * Created by ronnie on 2017. 8. 12.
+  */
+// Day 8: Dictionaries and Maps
+object DictionaryAndMaps {
+
+  def main(args: Array[String]): Unit = {
+    val N = scala.io.StdIn.readInt()
+
+    val phoneBook: Map[String, Long] = (1 to N).map(_ => scala.io.StdIn.readLine().split(" "))
+      .map(ar => (ar(0), ar(1).toLong))
+      .toMap
+
+    (1 to N).foreach(_ => {
+      Option(scala.io.StdIn.readLine()) match {
+        case Some(query) =>
+          val result = phoneBook.get(query) match {
+            case Some(phone) => s"$query=$phone"
+            case None => "Not found"
+          }
+          println(result)
+        case None => _
+      }
+    })
+  }
+}
