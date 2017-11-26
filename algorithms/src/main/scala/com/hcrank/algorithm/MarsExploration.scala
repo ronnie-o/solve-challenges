9c482ac5d3aa35e1728ef675a4d44465cd10b96b
ronnie.o <ronnie.o@kakaocorp.com>
2017-11-26 20:39:39 +0900

    [Algorithms] Mars Exploration

---
+++ algorithms/src/main/scala/com/hcrank/algorithm/MarsExploration.scala
@@ -0,0 +1,18 @@
+package com.hcrank.algorithm
+
+/**
+  * Created by ronnie on 2017. 11. 26.
+  * https://www.hackerrank.com/challenges/mars-exploration
+  */
+object MarsExploration {
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner (System.in)
+    var S = sc.next()
+
+    val SOS = "SOS"
+    val changed = (0 until S.length)
+      .filterNot(i => S.charAt(i) == SOS.charAt(i % 3))
+      .length
+    println(changed)
+  }
+}
