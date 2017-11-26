3fd4e047aaf21b876f28cf672cb0a1e20fff98fd
ronnie.o <ronnie.o@kakaocorp.com>
2017-11-26 21:20:38 +0900

    [Algorithms] Camelcase

---
+++ algorithms/src/main/scala/com/hcrank/algorithm/Camelcase.scala
@@ -0,0 +1,15 @@
+package com.hcrank.algorithm
+
+/**
+  * Created by ronnie on 2017. 11. 26.
+  * https://www.hackerrank.com/challenges/camelcase
+  */
+object Camelcase {
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner (System.in)
+    var s = sc.next()
+    
+    val count = if (s.length == 1) 1 else { s.filter(c => c.isUpper).length + 1 }
+    println(count)
+  }
+}
